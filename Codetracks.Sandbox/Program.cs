b48f5c7431d513bf121c26bebd54009fd115c54e
sgrechko <sgrechko@DT-TAL-000785>
2018-01-08 16:41:15 +0200

    ContractImplementation.

--- Codetracks.Sandbox/Program.cs
+++ Codetracks.Sandbox/Program.cs
@@ -10,12 +10,15 @@ namespace Codetracks.Sandbox
 		{
 			var shouldBePositivePredicate = Tuple.Create<Func<int, bool>, string>(i => i > 0, "Should be positive.");
 
-			MethodContract
+			var res = MethodContract
 				.Define
 				.Takes(shouldBePositivePredicate)
 				.Takes(shouldBePositivePredicate)
 				.Returns(shouldBePositivePredicate)
-				.Implement((leftValue, rightValue) => leftValue + rightValue);
+				.Implement((leftValue, rightValue) => leftValue + rightValue)
+				.Invoke
+					(1) // first arg
+					(1); // second arg
 
 			Console.Read();
 		}
