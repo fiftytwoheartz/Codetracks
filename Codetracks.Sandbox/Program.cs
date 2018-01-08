d29a17251e7f77ec4a81857d2559338078ec3880
sgrechko <sgrechko@DT-TAL-000785>
2018-01-08 17:56:03 +0200

    Sandbox example.

--- Codetracks.Sandbox/Program.cs
+++ Codetracks.Sandbox/Program.cs
@@ -8,16 +8,30 @@ namespace Codetracks.Sandbox
 	{
 		static void Main(string[] args)
 		{
-			MethodContract
-				.Define
-				.Takes(Predicates.Int32.Positive)
-				.Takes(Predicates.Int32.Positive)
-				.Returns(Predicates.Int32.Positive)
-				.Implement((leftValue, rightValue) => leftValue + rightValue)
-				.Invoke
-					(1) // first arg meets the requirement
-					(-1); // second arg violates the requirement
+			var message = string.Empty;
 
+			try
+			{
+				var res = 
+					MethodContract
+						.Define
+						.Takes(Predicates.Int32.Positive)
+						.Takes(Predicates.Int32.Positive)
+						.Returns(Predicates.Int32.Positive)
+						.Implement((leftValue, rightValue) => leftValue + rightValue)
+						.Invoke
+							(1) // first arg meets the requirement
+							(-1); // second arg violates the requirement
+
+				message = res.ToString();
+			}
+			catch (ArgumentException argumentException)
+			{
+				message = argumentException.Message;
+			}
+
+			Console.WriteLine(message);
+			
 			Console.Read();
 		}
 	}
